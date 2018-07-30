4bd3fd425a00bdb0a7276f3444e0eeba3408d95c
Kartik Pingale <kartikpgr8@gmail.com>
2018-07-30 20:03:25 +0530

    Create Assignment 6.c

---
+++ FPL 1/Assignment 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    
+    printf("Enter first side: ");
+    scanf("%d", &a);
+    printf("Enter second side: ");
+    scanf("%d", &b);
+    printf("Enter third side: ");
+    scanf("%d", &c);
+    
+    if(((a*a) == (b*b) + (c*c)) || ((b*b) == (a*a) + (c*c)) || ((c*c) == (b*b) + (a*a)))
+    {
+        printf("%d, %d, %d are Pythagorean triplets!", a, b, c);
+    }
+    else
+    {
+        printf("%d, %d, %d are not Pythagorean triplets!", a, b, c);
+    }
+    
+    return 0;
+}
