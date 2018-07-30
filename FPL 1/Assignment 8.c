c4e81fef80e78a4027d3e17d6f86febfa0d7134a
Kartik Pingale <kartikpgr8@gmail.com>
2018-07-30 20:04:49 +0530

    Create Assignment 8.c

---
+++ FPL 1/Assignment 8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, i, flag=0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    for(i=1; i<=number; i++)
+    {
+        if(number%i==0)
+        {
+            flag++;
+        }
+    }
+    if(flag==2)
+    {
+        printf("The number is prime!");
+    }
+    else
+    {
+        printf("The number is not prime!");
+    }
+    return 0;
+}
