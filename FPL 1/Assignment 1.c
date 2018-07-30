458e4fc786322a36cff289f21d08207d7467ae7f
Kartik Pingale <kartikpgr8@gmail.com>
2018-07-30 20:01:59 +0530

    Update Assignment 1.c

--- FPL 1/Assignment 1.c
+++ FPL 1/Assignment 1.c
@@ -1,2 +1,22 @@
-# FPL-1-2
-All the programs of FPL 1 &amp; 2 from first year engineering of SPPU
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, c;
+    float det, x, y;
+    
+    printf("Enter the coefficient of x^2: ");
+    scanf("%d", &a);
+    printf("Enter the coefficient of x: ");
+    scanf("%d", &b);
+    printf("Enter the constant term: ");
+    scanf("%d", &c);
+    
+    det = sqrt((b*b) - (4*a*c));
+    x = ((-b) + (det)) / (2*a);
+    y = ((-b) - (det)) / (2*a);
+    
+    printf("The roots of the given quadratic equation are %f and %f", x, y);
+    return 0;
+}
