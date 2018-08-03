c6876efe3c55659f9a26a5ced87c9f80cf6a67ef
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 21:53:05 +0530

    Update Assignment 4.c

--- FPL 1/Assignment 4.c
+++ FPL 1/Assignment 4.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    int sideA, sideB, sideC;
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &sideA, &sideB, &sideC);
+    if(sideA == sideB && sideB == sideC)
+    {
+        printf("The triangle is an equilateral triangle!");
+    }
+    else if(sideA == sideB || sideB == sideC || sideA == sideC)
+    {
+        printf("The triangle is an isosceles triangle!");
+    }
+    else if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==b*b+a*a)
+    {
+        printf("The triangle is a right angled triangle!");
+    }
+    else
+    {
+        printf("The triangle is a scalene triangle!");
+    }
+    return 0;
+}
