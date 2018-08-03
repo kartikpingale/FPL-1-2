35a63ea748ca968205454a3dee25cd70fc51e8f0
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 21:45:40 +0530

    Update Assignment 3.c

--- FPL 1/Assignment 3.c
+++ FPL 1/Assignment 3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    float price, discount, tax;
+    printf("Enter price of the laptop: ");
+    scanf("%f", &price);
+    discount = 0.15*price;
+    price = price-discount;
+    tax = 0.07*price;
+    printf("Discount: %0.2f\nEffective Price: %0.2f\nSales Tax: %0.2f\n", discount, price, tax);
+    return 0;
+}
