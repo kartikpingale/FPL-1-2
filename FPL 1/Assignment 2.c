494f37505796cb5e300eed966f2e0bc17d0c1845
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 21:40:11 +0530

    Update Assignment 2.c

--- FPL 1/Assignment 2.c
+++ FPL 1/Assignment 2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main()
+{
+    int number, evenSum=0, oddSum=0;
+    char repeat='y';
+    while(repeat=='Y' || repeat=='y')
+    {
+        printf("Enter a number: ");
+        scanf("%d", &number);
+        if(number%2==0)
+        {
+            evenSum+=number;
+        }
+        else
+        {
+            oddSum+=number;
+        }
+        printf("Do you want to enter another number? (y/n)");
+        scanf(" %c", &repeat);
+    }
+    printf("The sum of all the even numbers is %d, and that of odd numbers is %d\n", evenSum, oddSum);
+    return 0;
+}
