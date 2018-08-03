c793e1d51048dfc2054569476238eeb52301be24
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 22:00:17 +0530

    Update Assignment 5.c

--- FPL 1/Assignment 5.c
+++ FPL 1/Assignment 5.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main()
+{
+    float cost, sales, change, percentage;
+    printf("Enter cost price: ");
+    scanf("%f", &cost);
+    printf("Enter sales price: ");
+    scanf("%f", &sales);
+    if(sales>cost)
+    {
+        change=sales-cost;
+        percentage=(change/cost)*100;
+        printf("You have a profit of %0.2f and your profit percentage is %0.2f!\n", change, percentage);
+    }
+    else if(cost>sales)
+    {
+        change=cost-sales;
+        percentage=(change/cost)*100;
+        printf("You have a loss of %0.2f and your loss percentage is %0.2f!\n", change, percentage);
+    }
+    else
+    {
+        printf("You broke even!\n");
+    }
+    return 0;
+}
