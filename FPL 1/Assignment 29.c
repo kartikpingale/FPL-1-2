82ea2475e4e91799be7d937b26cfd312912fa43e
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 22:08:26 +0530

    Create Assignment 29.c

---
+++ FPL 1/Assignment 29.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char string[20];
+    char vowels[]={'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', '\0'};
+    int i, j;
+    printf("Enter a string: ");
+    scanf("%s", string);
+    for(i=0; string[i]!='\0'; i++)
+    {
+        for(j=0; vowels[j]!='\0'; j++)
+        {
+            if(string[i]==vowels[j])
+            {
+                string[i]='*';
+            }
+        }
+    }
+    printf("%s\n", string);
+    return 0;
+}
