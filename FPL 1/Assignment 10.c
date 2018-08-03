df4fd26c6e201446da5836901bb0c1362871d93d
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-03 22:43:40 +0530

    Update Assignment 10.c

--- FPL 1/Assignment 10.c
+++ FPL 1/Assignment 10.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+    int number, digit, newNumber=0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    while(number>0)
+    {
+        digit=number%10;
+        newNumber=newNumber*10+digit;
+        number=number/10;
+    }
+    while(newNumber>0)
+    {
+        digit=newNumber%10;
+        printf("%d\n", digit);
+        newNumber=newNumber/10;
+    }
+    return 0;
+}
