0b74f6935e3744038958358ef7f581cefb19836c
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-04 15:50:40 +0530

    Update Assignment 19.c

--- FPL 1/Assignment 19.c
+++ FPL 1/Assignment 19.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int array[20];
+    int size, i;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Reversed array:\n");
+    for(i=size-1; i>=0; i--)
+    {
+        printf("%d\n", array[i]);
+    }
+    return 0;
+}
