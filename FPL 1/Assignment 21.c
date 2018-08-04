d030d94c59d3fbdb856ab590f5a21f76cdd5840c
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-04 15:55:12 +0530

    Update Assignment 21.c

--- FPL 1/Assignment 21.c
+++ FPL 1/Assignment 21.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    int array[20], size, i, evenSum=0, oddSum=0;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    for(i=0; i<size; i++)
+    {
+        if(array[i]%2==0)
+        {
+            evenSum+=array[i];
+        }
+        else
+        {
+            oddSum+=array[i];
+        }
+    }
+    printf("The sum of all the even numbers is %d, and that of odd numbers is %d\n", evenSum, oddSum);
+    return 0;
+}
