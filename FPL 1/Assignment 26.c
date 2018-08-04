e606781035786557ab24a50a6e1c075f2e4f86a8
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-04 18:34:24 +0530

    Update Assignment 26.c

--- FPL 1/Assignment 26.c
+++ FPL 1/Assignment 26.c
@@ -1,1 +1,21 @@
+/*
+PROBLEM STATEMENT
+Write a C program that will check whether given string is palindrome or not.
+*/
 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char string[100], reversed[100];
+    if(strcmp(string,strrev(string))==0)
+    {
+        printf("Palindrome!");
+    }
+    else
+    {
+        printf("Not a palindrome!");
+    }
+    return 0;
+}
