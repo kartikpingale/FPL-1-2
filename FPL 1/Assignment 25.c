b4f5c451ea83bac44ea2f37493ae92c93e73b7b3
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:36:37 +0530

    Update Assignment 25.c

--- FPL 1/Assignment 25.c
+++ FPL 1/Assignment 25.c
@@ -1,1 +1,4 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to accept a string with complete name of the employee and display the string eliminating the middle word. Example “Mayur Kumar Kulkarni” would be displayed as “Mayur Kulkarni”
+*/
