08336a22af2b0ca24d98e13f64389eb8fdb6d7a3
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:17:06 +0530

    Update Assignment 17.c

--- FPL 1/Assignment 17.c
+++ FPL 1/Assignment 17.c
@@ -2,3 +2,26 @@
 PROBLEM STATEMENT
 Write a C program to store N numbers in an array and display the square of each number in the array
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int size, i;
+    int array[30];
+    
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    
+    for(i=0; i<size; i++)
+    {
+        printf("%d\n", (array[i]*array[i]));
+    }
+    
+    return 0;
+}
