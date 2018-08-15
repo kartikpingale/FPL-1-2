74ecbabd0819b475afa9f6a1a31dfa93ec80127c
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:00:14 +0530

    Update Assignment 20.c

--- FPL 1/Assignment 20.c
+++ FPL 1/Assignment 20.c
@@ -1,1 +1,4 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to store Chemistry subject test marks of N students in an array and find the Minimum and Maximum score. Test maximum marks= 20. Your program should accept marks ranging between 0 to 20 only.
+*/
