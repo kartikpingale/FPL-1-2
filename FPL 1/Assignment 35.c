6c43ec8e794a3a18337b03737d69f0bfff728521
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:34:38 +0530

    Update Assignment 35.c

--- FPL 1/Assignment 35.c
+++ FPL 1/Assignment 35.c
@@ -1,1 +1,4 @@
-
+/*
+PROBLEM STATEMENT
+A mall has 5 stores and 3 departments. Write a C program with function to find the monthly sale of a particular store or department and the total monthly sale of each store and each department.
+*/
