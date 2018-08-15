a269e8d9aaf6d48d652096363d8fac8986c3023b
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:32:58 +0530

    Update Assignment 31.c

--- FPL 1/Assignment 31.c
+++ FPL 1/Assignment 31.c
@@ -1,1 +1,7 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to carry out following operations on strings using library functions- a) To concatenate a string S2 to string S1
+b) To find the length of a given string
+c) To compare two strings S1 and S2
+d) To copy a string S2 to another string S1
+*/
