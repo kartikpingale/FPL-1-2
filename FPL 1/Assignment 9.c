61555ab3b7005c4d971a2184816e5093126337b4
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-15 18:06:01 +0530

    Update Assignment 9.c

--- FPL 1/Assignment 9.c
+++ FPL 1/Assignment 9.c
@@ -1,1 +1,8 @@
+/*
+PROBLEM STATEMENT
+Write a C program to check if (any one )
+a) The number is perfect number or not. Hint: If sum of the factors (including 1 and excluding the number itself) is same as that of original number then the number is said to be perfect number.
+b) The number is ‘neon’ number or not. Hint: If the sum of the digits of square of the number is the number itself, then the number is said to be neon. For example- 9, Square is 81 and 8+1=9.
+c) The number is ‘Armstrong’ number or not. Hint: If sum of cubes of digits is equal to original number then the number is said to be Armstrong. For example- Number 153 is Armstrong number (1)3+(5)3+(3)3=153
+*/
 
