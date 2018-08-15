1bd69d9f20b853baa477649401bde8eaa290921a
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:37:33 +0530

    Update Assignment 23.c

--- FPL 1/Assignment 23.c
+++ FPL 1/Assignment 23.c
@@ -1,1 +1,4 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to accept a string and change the case of each character of the string. Example “ THIs Is a C Program” changes to “thiS iS A c pROGRAM”
+*/
