fd1003897572ef9031c20f41bc2fcb2386a8180e
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:33:46 +0530

    Update Assignment 34.c

--- FPL 1/Assignment 34.c
+++ FPL 1/Assignment 34.c
@@ -1,1 +1,5 @@
 
+/*
+PROBLEM STATEMENT
+Write a C program that stores 12 city names in a single dimensional array. Write function to display only those words that begin with a consonant and end with a vowel example- Begaluru, Mumbai
+*/
