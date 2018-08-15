edf514ab22f199105cf51cd633d18d6cc14c8e15
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:37:59 +0530

    Update Assignment 22.c

--- FPL 1/Assignment 22.c
+++ FPL 1/Assignment 22.c
@@ -1,1 +1,7 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to accept a string and to display the following:
+(a) Total number of characters in the string
+(b) Total number of vowels in the string
+(c) Total number of occurrence of particular character in the string
+*/
