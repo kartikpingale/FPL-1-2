fe8f34760ea5d7bc93c893ee25dc15742252a007
Kartik Pingale <kartikpgr8@gmail.com>
2018-08-16 00:35:56 +0530

    Update Assignment 28.c

--- FPL 1/Assignment 28.c
+++ FPL 1/Assignment 28.c
@@ -1,1 +1,4 @@
-
+/*
+PROBLEM STATEMENT
+Write a C program to enter a string. Frame a word by joining all the first characters of the words in the string. Example- input “Just Allow Me” will give output as “JAM”
+*/
