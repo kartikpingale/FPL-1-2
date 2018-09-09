ecfa41ae7f60db3f46fc5524a0f493ea77358a83
Kartik Pingale <kartikpgr8@gmail.com>
2018-09-09 18:05:58 +0530

    Update Assignment 3.c

--- FPL 1/Assignment 3.c
+++ FPL 1/Assignment 3.c
@@ -8,11 +8,14 @@ A customer is offered 15% discount on the printed price of a laptop. The custome
 int main()
 {
     float price, discount, tax;
+    
     printf("Enter price of the laptop: ");
     scanf("%f", &price);
+    
     discount = 0.15*price;
     price = price-discount;
     tax = 0.07*price;
+    
     printf("Discount: %0.2f\nEffective Price: %0.2f\nSales Tax: %0.2f\n", discount, price, tax);
     return 0;
 }
