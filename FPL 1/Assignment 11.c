1bb11ef0070aeb351b6ea4c215e1ecdaaca0cb34
Kartik Pingale <kartikpgr8@gmail.com>
2018-09-09 18:08:25 +0530

    Update Assignment 11.c

--- FPL 1/Assignment 11.c
+++ FPL 1/Assignment 11.c
@@ -9,8 +9,10 @@ int main()
 {
     int count, fib, number, next_number;
     count=0, number=0, next_number=1;
+    
     printf("%d\n", number);
     printf("%d\n", next_number);
+    
     while(count<18)
     {
         fib = number + next_number;
@@ -19,5 +21,6 @@ int main()
         printf("%d\n", fib);
         count++;
     }
+    
     return 0;
 }
