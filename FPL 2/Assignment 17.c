f3e6d621962b06982ced54dafd4990ad2e2c5400
Kartik Pingale <kartikpgr8@gmail.com>
2019-03-06 19:29:21 +0530

    Update Assignment 17.c

--- FPL 2/Assignment 17.c
+++ FPL 2/Assignment 17.c
@@ -1,1 +1,16 @@
+package com.example.hello.helloworld;
 
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       Log.d("MainActivity", "Hello World");
+   }
+}
