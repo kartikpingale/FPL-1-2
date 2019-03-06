66a0eb17dd7f8d3d04e4c8ed93b6b471814860a3
Kartik Pingale <kartikpgr8@gmail.com>
2019-03-06 19:18:14 +0530

    Update Assignment 13.c

--- FPL 2/Assignment 13.c
+++ FPL 2/Assignment 13.c
@@ -1,1 +1,27 @@
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int a =0;
+int e = 7;
+
+void setup()
+{
+  	lcd.begin(16, 2);
+	lcd.print("Hello World!");
+	pinMode(e,INPUT);
+}
+
+void loop()
+{
+  	int button = digitalRead(e);
+	lcd.setCursor(3, 1);
+    lcd.print(a);
+  	
+	if(button==HIGH)
+	{
+    	a++;
+		lcd.setCursor(3, 1);
+		lcd.print(a);
+		delay(200);
+    }
+}
